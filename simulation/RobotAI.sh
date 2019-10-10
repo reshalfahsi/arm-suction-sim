55cdf01814c12c55b5b21343214a991d5c77f117
kevinaldian <kevinaldian@gitlab.com>
2019-10-10 09:27:22 +0700

    Little Adjustment

--- simulation/RobotAI.sh
+++ simulation/RobotAI.sh
@@ -7,8 +7,11 @@ sleep 7
 rostopic pub -1 /js_forward sensor_msgs/JointState '{position:[90,0,90,0,-90,0,1]}'
 sleep 7
 
-rostopic pub -1 /js_forward sensor_msgs/JointState '{position:[90,0,90,0,-90,0,0]}'
-sleep 7
+rostopic pub -1 /js_inverse sensor_msgs/JointState '{position:[-0.8870342373847961, -1.260109782218933, 0.16752795696258545, -0.4999183118343353, 0.5001394152641296, -0.5000441670417786, -0.49989810585975647, 1]}'
+sleep 3
+
+rostopic pub -1 /js_inverse sensor_msgs/JointState '{position:[-0.8870342373847961, -1.260109782218933, 0.16752795696258545, -0.4999183118343353, 0.5001394152641296, -0.5000441670417786, -0.49989810585975647, 0]}'
+sleep 3
 
 rostopic pub -1 /js_forward sensor_msgs/JointState '{position:[0,0,0,0,0,0,0]}'
 sleep 7
